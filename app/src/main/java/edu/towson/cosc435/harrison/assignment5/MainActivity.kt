c58e6dd0d970f97ce49604626f0664d44102d55f
JaVonne Harrison <javonneharrison@JaVonnes-MacBook-Pro.local>
2021-11-29 16:59:41 -0500

    this is the best I've got. I cant find any good resources online on how to put this all together

--- app/src/main/java/edu/towson/cosc435/harrison/assignment5/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc435/harrison/assignment5/MainActivity.kt
@@ -3,12 +3,20 @@ package edu.towson.cosc435.harrison.assignment5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.towson.cosc435.harrison.assignment5.ui.theme.Assignment5Theme
+import coil.compose.rememberImagePainter as composeRememberImagePainter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +24,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Assignment5Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val vm: RestAPIViewModel by viewModels {
+                    RestAPIViewModel.factory(application)
                 }
+                RestAPIScreen(vm)
             }
         }
     }
@@ -34,5 +43,53 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     Assignment5Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun RestAPIScreen(
+    vm: RestAPIViewModel
+) {
+//    LazyColumn(
+//        modifier = Modifier.fillMaxWidth()
+//    ) {
+//        items(vm.users.value) { user ->
+//            val painter = coil.compose.rememberImagePainter(
+//                data = user.thumbnailUrl,
+//                builder = {
+//                    crossfade(true)
+//                    placeholder(R.drawable.ic_launcher_foreground)
+//                },
+//            )
+//
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//            ) {
+//                Column(
+//                    modifier = Modifier
+//                        .height(128.dp)
+//                        .weight(1.0f),
+//                    verticalArrangement = Arrangement.Center,
+//                    horizontalAlignment = Alignment.CenterHorizontally
+//                ) {
+//                    Text(text = user.title, style = MaterialTheme.typography.h5)
+//                }
+//                LaunchedEffect(key1 = user.id) {
+//                    vm.fetchThumbnail(user.thumbnailUrl)
+//                }
+//                Box(
+//                    modifier = Modifier.weight(1.0f)
+//                ) {
+//                    Image(
+//                        modifier = Modifier.size(128.dp),
+//                        bitmap = user.thumbnail.value.asImageBitmap(), contentDescription = null)
+//                }
+//            }
+//        }
+//    }
+
+        //I cant figure out how to get the information from the view models and data classes to put in here
+
     }
 }
--- app/src/main/java/edu/towson/cosc435/harrison/assignment5/PhotoUser.kt
+++ app/src/main/java/edu/towson/cosc435/harrison/assignment5/PhotoUser.kt
@@ -6,6 +6,10 @@ import android.graphics.BitmapFactory
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key.Companion.T
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.*
 import androidx.room.*
 import com.google.gson.Gson
@@ -21,6 +25,9 @@ import java.io.File
 
 interface IPhotoUsersFetcher {
     suspend fun fetchTitle() : String
+    suspend fun fetchUsers() : List<PhotoUserI>
+    suspend fun fetchThumbnailUrl() : String
+    suspend fun fetchUrl() : String
     suspend fun fetchThumbnail(thumbnail_url: String) : Bitmap
     suspend fun fetchPicture(url_ : String) : Bitmap
 }
@@ -34,7 +41,46 @@ class UsersFetcher(private val cacheDir: File) : IPhotoUsersFetcher {
             val response = client.newCall(request).execute()
             val body = response.body?.string()
             val apiResult = Gson().fromJson(body, ApiResult::class.java)
+
             apiResult.title_
+        }
+    }
+
+    override suspend fun fetchUsers(): List<PhotoUserI> {
+        return withContext(Dispatchers.IO) {
+            val client = OkHttpClient()
+            val request = Request.Builder().get().url("${API_URL}?.albumId=1")
+                .build()
+            val response = client.newCall(request).execute()
+            val body = response.body?.string()
+            val apiResult = Gson().fromJson(body, ApiResult::class.java)
+            listOf(PhotoUserI(apiResult.album_Id, apiResult.id_, apiResult.title_, apiResult.url_, apiResult.thumbnail_Url, fetchThumbnail(apiResult.thumbnail_Url)))
+        }
+    }
+
+    override suspend fun fetchThumbnailUrl() : String {
+        return withContext(Dispatchers.IO) {
+            val client = OkHttpClient()
+            val request = Request.Builder().get().url("${API_URL}?.albumId=1")
+                .build()
+            val response = client.newCall(request).execute()
+            val body = response.body?.string()
+            val apiResult = Gson().fromJson(body, ApiResult::class.java)
+
+            apiResult.thumbnail_Url
+        }
+    }
+
+    override suspend fun fetchUrl() : String {
+        return withContext(Dispatchers.IO) {
+            val client = OkHttpClient()
+            val request = Request.Builder().get().url("${API_URL}?.albumId=1")
+                .build()
+            val response = client.newCall(request).execute()
+            val body = response.body?.string()
+            val apiResult = Gson().fromJson(body, ApiResult::class.java)
+
+            apiResult.url_
         }
     }
 
@@ -84,7 +130,7 @@ data class PhotoUserI(
     val albumId: Int,
 
     @SerializedName("id")
-    val id: Int,
+    val user_id: Int,
 
     @SerializedName("title")
     val title: String,
@@ -93,33 +139,72 @@ data class PhotoUserI(
     val url: String,
 
     @SerializedName("thumbnailUrl")
-    val thumbnailUrl : String
+    val thumbnailUrl: String,
+    var thumbnail: Bitmap
+//    val photo: MutableState<Bitmap>
 )
+
 
 class RestAPIViewModel(app : Application) : AndroidViewModel(app){
     private val _title: MutableState<String>
     val title: State<String>
 
-    private val _thumbnailUrl: MutableState<String>
-    val thumbnailUrl: State<String>
+    private val _thumbnail : MutableState<Bitmap?>
+    val thumbnail : State<Bitmap?>
 
-    private val _url: MutableState<String>
-    val url: State<String>
+    private var _thumbnail_url : MutableState<String>
+    var thumbnail_url : State<String>
+
+    private var _users : MutableState<List<PhotoUserI>>
+    var users : State<List<PhotoUserI>>
+
 
 
 
     val usersFetcher: IPhotoUsersFetcher = UsersFetcher(getApplication<Application>().cacheDir)
 
     init{
+
+        _users = mutableStateOf(listOf())
+        users = _users
+
         _title = mutableStateOf("")
         title = _title
-        _thumbnailUrl = mutableStateOf("")
-        thumbnailUrl = _thumbnailUrl
-        _url = mutableStateOf("")
-        url = _url
+
+        _thumbnail = mutableStateOf<Bitmap?>(null)
+        thumbnail = _thumbnail
+
+        _thumbnail_url = mutableStateOf("")
+        thumbnail_url = _thumbnail_url
+
+
+//        viewModelScope.launch {
+//            fetchTitle()
+//        }
+//        viewModelScope.launch {
+//            fetchThumbnail(thumbnail_url.toString())
+//        }
         viewModelScope.launch {
-            fetchTitle()
+            fetchUsers()
         }
+
+    }
+
+    suspend fun fetchUsers(){
+        val users = usersFetcher.fetchUsers()
+        users.forEach { user ->
+            user.thumbnail =
+                getApplication<Application>()
+                    .theme
+                    .getDrawable(R.drawable.ic_launcher_foreground)
+                    .toBitmap()
+        }
+        _users.value = users
+    }
+
+    suspend fun fetchThumbnailUrl(){
+        val thumbnailUrl = usersFetcher.fetchThumbnailUrl()
+        _thumbnail_url = mutableStateOf(thumbnailUrl)
     }
 
     suspend fun fetchTitle(){
@@ -127,8 +212,20 @@ class RestAPIViewModel(app : Application) : AndroidViewModel(app){
         _title.value = title
     }
 
-    suspend fun fetchThumbnail(user : PhotoUserI) {
+    suspend fun fetchThumbnail(url: String) {
+        val thumbnail = usersFetcher.fetchThumbnail(url)
+        _thumbnail.value = thumbnail
+    }
 
+    companion object {
+        fun factory(app: Application): ViewModelProvider.Factory {
+            return object: ViewModelProvider.Factory {
+                override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                    return RestAPIViewModel(app) as T
+                }
+
+            }
+        }
     }
 
 }
@@ -136,12 +233,13 @@ class RestAPIViewModel(app : Application) : AndroidViewModel(app){
 data class ApiResult(val album_Id: Int, val id_: Int, val title_: String, val url_: String,
                      val thumbnail_Url: String)
 
+
 @Entity
 data class PhotoUser (
     @PrimaryKey(autoGenerate = true)
-    var Id: Int,
+    var key: Int = 0,
     @ColumnInfo(name = "albumId") var album_Id: String,
-    @ColumnInfo(name = "id") var id: String,
+    @ColumnInfo(name = "id") var song_id: String,
     @ColumnInfo(name = "title") var title: String,
     @ColumnInfo(name = "url") var url: String,
     @ColumnInfo(name = "thumbnailUrl") var thumbnailUrl: String
